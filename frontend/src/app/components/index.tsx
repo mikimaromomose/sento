fbdadd3819876c12e842888fd0075bdbea974479
大槻 譲二 <jootsuki@corron.jp>
2024-10-31 02:05:48 +0900

    index追加(メニューコンポーネント)

--- frontend/src/app/components/index.tsx
+++ frontend/src/app/components/index.tsx
@@ -1,11 +1,13 @@
 import { Button } from './button';
 import Header from './header';
 import Loading from './loading';
+import Menu from './menu';
 
 const components = {
   Button,
   Header,
-  Loading
+  Loading,
+  Menu
 };
 
 export default components;
