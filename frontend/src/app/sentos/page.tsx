65fa5a2e924ceee5161e319742bc91253b639f62
LUNAYAM-2024\yamaz <ribenlucy12@gmail.com>
2024-12-22 11:26:51 +0900

    1222最新版です。

--- frontend/src/app/sentos/page.tsx
+++ frontend/src/app/sentos/page.tsx
@@ -12,6 +12,11 @@ import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import { motion } from "framer-motion";
 import Loading from "../components/loading";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
 export default function Sentos() {
   const router = useRouter();
@@ -23,7 +28,12 @@ export default function Sentos() {
             "nearest_station": "高円寺",
             "walking_time": 5,
             "address": "東京都杉並区高円寺北３丁目３２−１７",
-            "operating_hours_remarks": null
+            "operating_hours_remarks": null,
+            "images": [
+              "小杉湯.jpeg",
+              "小杉湯.jpeg",
+              "小杉湯.jpeg",
+            ]
         },
       {
             "id": "4a55efdd-ad25-48b7-83b7-b5ffabcbf082",
@@ -31,7 +41,12 @@ export default function Sentos() {
             "nearest_station": "高円寺",
             "walking_time": 5,
             "address": "東京都杉並区高円寺北2丁目",
-            "operating_hours_remarks": null
+            "operating_hours_remarks": null,
+            "images": [
+              "小杉湯.jpeg",
+              "小杉湯.jpeg",
+              "小杉湯.jpeg",
+            ]
         },
         {
             "id": "3a55efdd-ad25-48b7-83b7-b5ffabcbf082",
@@ -39,7 +54,12 @@ export default function Sentos() {
             "nearest_station": "都立家政",
             "walking_time": 5,
             "address": "東京都中野区",
-            "operating_hours_remarks": null
+            "operating_hours_remarks": null,
+            "images": [
+              "小杉湯.jpeg",
+              "小杉湯.jpeg",
+              "小杉湯.jpeg",
+            ]
         },
         {
             "id": "2a55efdd-ad25-48b7-83b7-b5ffabcbf082",
@@ -47,7 +67,10 @@ export default function Sentos() {
             "nearest_station": "中野",
             "walking_time": 5,
             "address": "東京都中野区１７",
-            "operating_hours_remarks": null
+            "operating_hours_remarks": null,
+            "images": [
+              "小杉湯.jpeg"
+            ]
         },
         {
           "id": "2a55efdd-ad25-48b7-83b7-b5ffabcbf044",
@@ -55,7 +78,11 @@ export default function Sentos() {
           "nearest_station": "長野県",
           "walking_time": 3,
           "address": "かるいざわ",
-          "operating_hours_remarks": null
+          "operating_hours_remarks": null,
+          "images": [
+            "小杉湯.jpeg",
+            "小杉湯.jpeg"
+          ]
       }
     ]
   const [loading, setLoading] = useState(true);
@@ -87,26 +114,78 @@ export default function Sentos() {
     return (<Loading loading={loading} paddingTop={80}/>)
   }
 
+   // Sliderの設定
+    const NextArrow = ({ className, style, onClick }) => {
+      return (
+        <div
+          className={className}
+          style={{ ...style, display: "block", position: 'absolute', top: '50%', right: '10px', 
+            zIndex: '1', cursor: 'pointer' , right: '-3px'}} 
+            onClick={onClick}
+        >
+       </div>
+     );
+    };
+      
+    const PrevArrow = ({className, style, onClick }) => {
+      return (
+        <div
+          className={className}
+          style={{ ...style, display: "block", position: 'absolute', top: '50%', left: '10px', 
+           zIndex: '1', cursor: 'pointer' ,left: '-3px'}}
+          onClick={onClick}
+        >
+        </div>
+      );
+    };
+         
+
+    const settings = {
+      infinite: false,
+      speed: 500,
+      slidesToShow: 1, 
+      slidesToScroll: 1,
+      nextArrow: <NextArrow />,  
+      prevArrow: <PrevArrow />   
+    };
+
+    
   return (
     <div style={{ backgroundColor: 'white' }}>
-      <header style={{ backgroundColor: '#D4AF37', padding: '60px' }}>
-        <h1 style={{ margin: '0', textAlign: 'center' ,color: 'white' }}>セントウ</h1>
-        <h1 style={{ margin: '0', textAlign: 'center' ,color: 'white' }}>ツカリタイ</h1>
+      <header style={{ backgroundColor: '#D4AF37', padding: '70px' }}>
+      <img
+        src="タイトル-3-横タイトル白.png"  // Path to your PNG image
+        alt="Header Image"
+        style={{
+          width: '100%', // 画像幅を画面幅に合わせる（スマホでも全幅）
+          maxWidth: '500px', // 最大幅を指定（これで大きすぎないように調整）
+          height: 'auto', // アスペクト比を保つ
+          marginBottom: '20px', // 画像とテキストの間にスペース
+          display: 'block', // 画像をブロック要素として扱い、中央に配置
+          marginLeft: 'auto', // 左マージン自動
+          marginRight: 'auto', // 右マージン自動
+       }}
+      />
       </header>
       <Grid container spacing={2}>
         {sentos.map((sento) => (
           <Grid item xs={12} sm={6} md={4} key={sento.id}>
-            <Card style={{ backgroundColor: 'white' }}>
+            <Card style={{ backgroundColor: 'white' , margin: '10px' }}>
               <Grid container>
-                <Grid item xs={4}>
-                  <CardMedia
-                    component="img"
-                    height="140"
-                    image="小杉湯.jpeg"
-                    alt="銭湯の画像"
-                  />
+                <Grid item xs={5}>
+                <Slider {...settings}>
+                  {sento.images.map((image, index) => (
+                    <CardMedia
+                      key={index}
+                      component="img"
+                      height="140"
+                      image={image || "default-image.jpeg"} // 銭湯の画像URLを指定
+                      alt={`${sento.name}の画像`}
+                    />
+                  ))}
+                </Slider>
                 </Grid>
-                <Grid item xs={8}>
+                <Grid item xs={7}>
                   <CardContent>
                     <Typography variant="h5" component="h2">
                       {sento.name}
@@ -120,7 +199,9 @@ export default function Sentos() {
                     <div style={{ textAlign: 'right', marginTop: '16px' }}>
                      <Button
                          variant="contained"
-                          style={{ backgroundColor: '#D4AF37', color: 'white', marginBottom: '14px', width: '70%', borderRadius: '10px' }} 
+                          style={{ backgroundColor: '#D4AF37', color: 'white', 
+                            marginBottom: '14px', width: '70%', borderRadius: '12px',
+                             transition: 'background-color 0.3s'}}
                           onClick={() => handleDetailClick(sento.id)}
                        >
                         詳細
@@ -135,4 +216,5 @@ export default function Sentos() {
       </Grid>
     </div>
   );
+
 }
